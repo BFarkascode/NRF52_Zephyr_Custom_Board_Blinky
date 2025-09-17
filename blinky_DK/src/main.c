b3bddef76aaba6315d41e5ddfa7090cc57ae1d8a
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2025-09-17 17:07:54 +0200

    Update main.c

--- blinky_DK/src/main.c
+++ blinky_DK/src/main.c
@@ -24,46 +24,6 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);					/
 
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
 
-/*static struct gpio_callback button_cb_data;													//we define a callback handle
-
-void button_push(const struct device *dev, struct gpio_callback *cb, uint32_t pins)			//we define the callback function
-{
-	gpio_pin_toggle_dt(&led);
-}
-
-int main(void)
-{
-	int ret;
-
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT | GPIO_ACTIVE_HIGH);					//we turn on the LED from the start
-	if (ret < 0) {
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);									//confiugre button GPIO
-
-	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);			//configure interrupt on the button
-	if (ret < 0) {
-		return 0;
-	}
-
-	gpio_init_callback(&button_cb_data, button_push, BIT(button.pin));					//we init the callback function on the desired parameters
-
-	gpio_add_callback(button.port, &button_cb_data);									//we start the interrupt
-
-	while (1) {
-
-		k_msleep(1000);
-
-	}
-	return 0;
-}*/
-
-
 int main(void)
 {
 	int ret;
